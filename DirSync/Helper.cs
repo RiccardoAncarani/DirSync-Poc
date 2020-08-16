7196d9be926b643e35f3f326525145250ecd3ce1
Riccardo Ancarani <RiccardoAncarani@users.noreply.github.com>
2020-08-16 18:16:59 +0100

    first commit

---
+++ DirSync/Helper.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+
+namespace DirSync
+{
+  
+    internal class MappedPrincipal
+    {
+        private string _principalName;
+        private string _objectType;
+
+        public string PrincipalName
+        {
+            get => _principalName.ToUpper();
+            set => _principalName = value;
+        }
+
+        public string ObjectType
+        {
+            get => _objectType.ToLower();
+            set => _objectType = value;
+        }
+
+        public MappedPrincipal(string name, string type)
+        {
+            PrincipalName = name;
+            ObjectType = type;
+        }
+
+        public static bool GetCommon(string sid, out MappedPrincipal result)
+        {
+            switch (sid)
+            {
+                case "S-1-0":
+                    result = new MappedPrincipal("Null Authority", "USER");
+                    break;
+                case "S-1-0-0":
+                    result = new MappedPrincipal("Nobody", "USER");
+                    break;
+                case "S-1-1":
+                    result = new MappedPrincipal("World Authority", "USER");
+                    break;
+                case "S-1-1-0":
+                    result = new MappedPrincipal("Everyone", "GROUP");
+                    break;
+                case "S-1-2":
+                    result = new MappedPrincipal("Local Authority", "USER");
+                    break;
+                case "S-1-2-0":
+                    result = new MappedPrincipal("Local", "GROUP");
+                    break;
+                case "S-1-2-1":
+                    result = new MappedPrincipal("Console Logon", "GROUP");
+                    break;
+                case "S-1-3":
+                    result = new MappedPrincipal("Creator Authority", "USER");
+                    break;
+                case "S-1-3-0":
+                    result = new MappedPrincipal("Creator Owner", "USER");
+                    break;
+                case "S-1-3-1":
+                    result = new MappedPrincipal("Creator Group", "GROUP");
+                    break;
+                case "S-1-3-2":
+                    result = new MappedPrincipal("Creator Owner Server", "COMPUTER");
+                    break;
+                case "S-1-3-3":
+                    result = new MappedPrincipal("Creator Group Server", "COMPUTER");
+                    break;
+                case "S-1-3-4":
+                    result = new MappedPrincipal("Owner Rights", "GROUP");
+                    break;
+                case "S-1-4":
+                    result = new MappedPrincipal("Non-unique Authority", "USER");
+                    break;
+                case "S-1-5":
+                    result = new MappedPrincipal("NT Authority", "USER");
+                    break;
+                case "S-1-5-1":
+                    result = new MappedPrincipal("Dialup", "GROUP");
+                    break;
+                case "S-1-5-2":
+                    result = new MappedPrincipal("Network", "GROUP");
+                    break;
+                case "S-1-5-3":
+                    result = new MappedPrincipal("Batch", "GROUP");
+                    break;
+                case "S-1-5-4":
+                    result = new MappedPrincipal("Interactive", "GROUP");
+                    break;
+                case "S-1-5-6":
+                    result = new MappedPrincipal("Service", "GROUP");
+                    break;
+                case "S-1-5-7":
+                    result = new MappedPrincipal("Anonymous", "GROUP");
+                    break;
+                case "S-1-5-8":
+                    result = new MappedPrincipal("Proxy", "GROUP");
+                    break;
+                case "S-1-5-9":
+                    result = new MappedPrincipal("Enterprise Domain Controllers", "GROUP");
+                    break;
+                case "S-1-5-10":
+                    result = new MappedPrincipal("Principal Self", "USER");
+                    break;
+                case "S-1-5-11":
+                    result = new MappedPrincipal("Authenticated Users", "GROUP");
+                    break;
+                case "S-1-5-12":
+                    result = new MappedPrincipal("Restricted Code", "GROUP");
+                    break;
+                case "S-1-5-13":
+                    result = new MappedPrincipal("Terminal Server Users", "GROUP");
+                    break;
+                case "S-1-5-14":
+                    result = new MappedPrincipal("Remote Interactive Logon", "GROUP");
+                    break;
+                case "S-1-5-15":
+                    result = new MappedPrincipal("This Organization ", "GROUP");
+                    break;
+                case "S-1-5-17":
+                    result = new MappedPrincipal("This Organization ", "GROUP");
+                    break;
+                case "S-1-5-18":
+                    result = new MappedPrincipal("Local System", "USER");
+                    break;
+                case "S-1-5-19":
+                    result = new MappedPrincipal("NT Authority", "USER");
+                    break;
+                case "S-1-5-20":
+                    result = new MappedPrincipal("NT Authority", "USER");
+                    break;
+                case "S-1-5-80-0":
+                    result = new MappedPrincipal("All Services ", "GROUP");
+                    break;
+                case "S-1-5-32-544":
+                    result = new MappedPrincipal("Administrators", "GROUP");
+                    break;
+                case "S-1-5-32-545":
+                    result = new MappedPrincipal("Users", "GROUP");
+                    break;
+                case "S-1-5-32-546":
+                    result = new MappedPrincipal("Guests", "GROUP");
+                    break;
+                case "S-1-5-32-547":
+                    result = new MappedPrincipal("Power Users", "GROUP");
+                    break;
+                case "S-1-5-32-548":
+                    result = new MappedPrincipal("Account Operators", "GROUP");
+                    break;
+                case "S-1-5-32-549":
+                    result = new MappedPrincipal("Server Operators", "GROUP");
+                    break;
+                case "S-1-5-32-550":
+                    result = new MappedPrincipal("Print Operators", "GROUP");
+                    break;
+                case "S-1-5-32-551":
+                    result = new MappedPrincipal("Backup Operators", "GROUP");
+                    break;
+                case "S-1-5-32-552":
+                    result = new MappedPrincipal("Replicators", "GROUP");
+                    break;
+                case "S-1-5-32-554":
+                    result = new MappedPrincipal("Pre-Windows 2000 Compatible Access", "GROUP");
+                    break;
+                case "S-1-5-32-555":
+                    result = new MappedPrincipal("Remote Desktop Users", "GROUP");
+                    break;
+                case "S-1-5-32-556":
+                    result = new MappedPrincipal("Network Configuration Operators", "GROUP");
+                    break;
+                case "S-1-5-32-557":
+                    result = new MappedPrincipal("Incoming Forest Trust Builders", "GROUP");
+                    break;
+                case "S-1-5-32-558":
+                    result = new MappedPrincipal("Performance Monitor Users", "GROUP");
+                    break;
+                case "S-1-5-32-559":
+                    result = new MappedPrincipal("Performance Log Users", "GROUP");
+                    break;
+                case "S-1-5-32-560":
+                    result = new MappedPrincipal("Windows Authorization Access Group", "GROUP");
+                    break;
+                case "S-1-5-32-561":
+                    result = new MappedPrincipal("Terminal Server License Servers", "GROUP");
+                    break;
+                case "S-1-5-32-562":
+                    result = new MappedPrincipal("Distributed COM Users", "GROUP");
+                    break;
+                case "S-1-5-32-568":
+                    result = new MappedPrincipal("IIS_IUSRS", "GROUP");
+                    break;
+                case "S-1-5-32-569":
+                    result = new MappedPrincipal("Cryptographic Operators", "GROUP");
+                    break;
+                case "S-1-5-32-573":
+                    result = new MappedPrincipal("Event Log Readers", "GROUP");
+                    break;
+                case "S-1-5-32-574":
+                    result = new MappedPrincipal("Certificate Service DCOM Access", "GROUP");
+                    break;
+                case "S-1-5-32-575":
+                    result = new MappedPrincipal("RDS Remote Access Servers", "GROUP");
+                    break;
+                case "S-1-5-32-576":
+                    result = new MappedPrincipal("RDS Endpoint Servers", "GROUP");
+                    break;
+                case "S-1-5-32-577":
+                    result = new MappedPrincipal("RDS Management Servers", "GROUP");
+                    break;
+                case "S-1-5-32-578":
+                    result = new MappedPrincipal("Hyper-V Administrators", "GROUP");
+                    break;
+                case "S-1-5-32-579":
+                    result = new MappedPrincipal("Access Control Assistance Operators", "GROUP");
+                    break;
+                case "S-1-5-32-580":
+                    result = new MappedPrincipal("Remote Management Users", "GROUP");
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+            return result != null;
+        }
+    }
+    internal class ACL : IEquatable<ACL>
+    {
+        public string PrincipalName { get; set; }
+        public string PrincipalType { get; set; }
+        public string RightName { get; set; }
+
+        public string AceType { get; set; }
+        //public string Qualifier { get; set; }
+        //public bool Inherited { get; set; }
+
+        public bool Equals(ACL other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(PrincipalName, other.PrincipalName) &&
+                   string.Equals(PrincipalType, other.PrincipalType) && string.Equals(RightName, other.RightName) &&
+                   string.Equals(AceType, other.AceType);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((ACL)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = PrincipalName != null ? PrincipalName.GetHashCode() : 0;
+                hashCode = (hashCode * 397) ^ (PrincipalType != null ? PrincipalType.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (RightName != null ? RightName.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (AceType != null ? AceType.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ DirSync/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Principal;
+using System.Collections.Concurrent;
+using System.Security.AccessControl;
+using System.Web.Script.Serialization;
+
+namespace DirSync
+{
+    class Program
+    {
+        private static ConcurrentDictionary<string, byte> _nullSids;
+        private static readonly string[] Props = { "distinguishedname", "samaccounttype", "samaccountname", "dnshostname" };
+        private static ConcurrentDictionary<string, string> _guidMap;
+        private static ConcurrentDictionary<string, string> _baseGuids;
+        private static readonly string AllGuid = "00000000-0000-0000-0000-000000000000";
+
+        static void Main(string[] args)
+        {
+            string ldapCookie = "adsync-cookie.dat";
+            string str_dcName = "dc01.isengard.local";
+            bool firstRun = true;
+
+            _nullSids = new ConcurrentDictionary<string, byte>();
+            _guidMap = new ConcurrentDictionary<string, string>();
+            _baseGuids = new ConcurrentDictionary<string, string>();
+            _baseGuids.TryAdd("user", "bf967aba-0de6-11d0-a285-00aa003049e2");
+            _baseGuids.TryAdd("computer", "bf967a86-0de6-11d0-a285-00aa003049e2");
+            _baseGuids.TryAdd("group", "bf967a9c-0de6-11d0-a285-00aa003049e2");
+            _baseGuids.TryAdd("domain", "19195a5a-6da0-11d0-afd3-00c04fd930c9");
+            _baseGuids.TryAdd("gpo", "f30e3bc2-9ff0-11d1-b603-0000f80367c1");
+
+            System.DirectoryServices.DirectoryEntry rootDSE = new System.DirectoryServices.DirectoryEntry("LDAP://rootDSE");
+            System.Net.NetworkCredential cr = new System.Net.NetworkCredential(@"Administrator", "1qazxsw2..", "isengard.local");
+            LdapConnection connection = new LdapConnection(str_dcName);
+            connection.Credential = cr;
+            connection.Bind();
+
+            DirectorySynchronization sync = new DirectorySynchronization();
+            DirectorySearcher src2 = new DirectorySearcher();
+
+            if (File.Exists(ldapCookie))
+            {
+                byte[] byteCookie = File.ReadAllBytes(ldapCookie);
+                sync.ResetDirectorySynchronizationCookie(byteCookie);
+                
+                firstRun = false;
+            }
+            src2.DirectorySynchronization = sync;
+
+            foreach (SearchResult res in src2.FindAll())
+                {
+                    ResultPropertyCollection fields = res.Properties;
+
+                    foreach (String ldapField in fields.PropertyNames)
+                    {
+
+                        foreach (Object myCollection in fields[ldapField])
+                        {
+                        if (!firstRun)
+                        {
+                            if (ldapField == "distinguishedname")
+                            {
+                                Console.WriteLine(String.Format("[+] DN = {0}", myCollection));
+
+                            }
+
+                            if (ldapField == "ntsecuritydescriptor")
+                            {
+                                Console.WriteLine("[+] Detected ACL Change: ");
+                                var aces = new List<ACL>();
+                                var newDescriptor = new ActiveDirectorySecurity();
+                                newDescriptor.SetSecurityDescriptorBinaryForm((byte[])myCollection);
+                               // todo add owner 
+
+                                foreach (ActiveDirectoryAccessRule ace in newDescriptor.GetAccessRules(true, true, typeof(SecurityIdentifier)))
+                                {
+                                    //Ignore null aces
+                                    if (ace == null)
+                                        continue;
+
+                                    //Ignore Deny aces
+                                    if (!ace.AccessControlType.Equals(AccessControlType.Allow))
+                                        continue;
+
+                                    //Resolve the principal in the ACE
+                                    var principal = GetAcePrincipal(ace, "isengard.local");
+                                    string name = new System.Security.Principal.SecurityIdentifier(principal).Translate(typeof(System.Security.Principal.NTAccount)).ToString();
+
+                                    //If its null, we don't care so move on
+                                    if (principal == null)
+                                        continue;
+
+                                   
+                                    //Interesting Domain ACEs - GenericAll, WriteDacl, WriteOwner, Replication Rights, AllExtendedRights
+                                    var rights = ace.ActiveDirectoryRights;
+                                    var objectAceType = ace.ObjectType.ToString();
+
+                                    if (rights.HasFlag(ActiveDirectoryRights.GenericAll))
+                                    {
+                                        if (objectAceType == AllGuid || objectAceType == "")
+                                        {
+                                            aces.Add(new ACL
+                                            {
+                                                AceType = "",
+                                                RightName = "GenericAll",
+                                                PrincipalName = name,
+                                                PrincipalType = principal
+                                            });
+                                        }
+                                        //GenericAll includes every other flag, so continue here so we don't duplicate privs
+                                        continue;
+                                    }
+
+                                    if (rights.HasFlag(ActiveDirectoryRights.WriteDacl))
+                                    {
+                                        aces.Add(new ACL
+                                        {
+                                            AceType = "",
+                                            RightName = "WriteDacl",
+                                            PrincipalName = name,
+                                            PrincipalType = principal
+                                        });
+                                    }
+
+                                    if (rights.HasFlag(ActiveDirectoryRights.WriteOwner))
+                                    {
+                                        aces.Add(new ACL
+                                        {
+                                            AceType = "",
+                                            RightName = "WriteOwner",
+                                            PrincipalName = name,
+                                            PrincipalType = principal
+                                        });
+                                    }
+
+                                    if (rights.HasFlag(ActiveDirectoryRights.ExtendedRight))
+                                    {
+                                        if (objectAceType == "1131f6aa-9c07-11d1-f79f-00c04fc2dcd2")
+                                        {
+                                            aces.Add(new ACL
+                                            {
+                                                AceType = "GetChanges",
+                                                RightName = "ExtendedRight",
+                                                PrincipalName = name,
+                                                PrincipalType = principal
+                                            });
+                                        }
+                                        else if (objectAceType == "1131f6ad-9c07-11d1-f79f-00c04fc2dcd2")
+                                        {
+                                            aces.Add(new ACL
+                                            {
+                                                AceType = "GetChangesAll",
+                                                RightName = "ExtendedRight",
+                                                PrincipalName = name,
+                                                PrincipalType = principal
+                                            });
+                                        }
+                                        else if (objectAceType == AllGuid || objectAceType == "")
+                                        {
+                                            aces.Add(new ACL
+                                            {
+                                                AceType = "All",
+                                                RightName = "ExtendedRight",
+                                                PrincipalName = name,
+                                                PrincipalType = principal
+                                            });
+                                        }
+                                    }
+                                }
+                                
+
+                                foreach (var ace in aces)
+                                {
+                                    
+                                    Console.WriteLine(String.Format("[+] {0} has {1}", ace.PrincipalName, ace.RightName));
+                                }
+                                ;
+                            }
+
+
+                            if (ldapField == "useraccountcontrol")
+                            {
+                                Console.WriteLine(String.Format("[+] UAC edited: {0}", myCollection));
+                            }
+                        }
+                           
+                        }
+                    }
+                }
+
+            File.WriteAllBytes(ldapCookie, sync.GetDirectorySynchronizationCookie());
+
+
+
+        }
+
+        private static string GetAcePrincipal(ActiveDirectoryAccessRule rule, string domainName)
+        {
+            return rule.IdentityReference.Value;
+        }
+    }
+}
